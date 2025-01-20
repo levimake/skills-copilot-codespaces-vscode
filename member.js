23f2d7347a47e81e4b760f2f07e3ad95e82915ab
levimake <levimake10@gmail.com>
2025-01-20 16:29:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {  
+  var skills = ["HTML", "CSS", "JavaScript", "Python"];
+  var member = {
+    name: "Sally",
+    age: 35,
+    skills: skills
+  };
+  return member;  
+}
+    
